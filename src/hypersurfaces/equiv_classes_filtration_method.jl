8950841bce85cbb4feeabfa93e5eadeb424cd989
Eoin Mackall <eoinmackall@gmail.com>
2025-11-12 23:29:06 -0800

    Seems complete

--- src/hypersurfaces/equiv_classes_filtration_method.jl
+++ src/hypersurfaces/equiv_classes_filtration_method.jl
@@ -134,7 +134,7 @@ function _chain_finder(head::DChainNode)
             good_chain = chain
         end
         if temp_d == d
-            good_chain = length(chain)<length(good_chain) ? chain : good_chain
+            good_chain = length(chain)>length(good_chain) ? chain : good_chain
         end
     end
     return (d, good_chain)
@@ -321,60 +321,95 @@ end
 #
 #####################################################################
 
-function is_stabilizing(A, vec, preim, p, inc, inv_inc)
-        # Function to check if a matrix A stabilizes a vector vec
-        
-        
-        
+function is_stabilizing(A, vec, preim_poly, p, x, inv_poly)
+    # Function to check if a matrix A stabilizes a vector vec
+
+    y = A*x
+    h=preim_poly(y...)
+    h_vec=inv_poly(h)
+    if vec == p(h_vec)
+        return true
+    else
+        return false
+    end
 end
 
-function lift_orbit_representatives(ImV_i, W_i, p, q, inc, inv_inc, f_i, orbits, GL_vec)
-    R=codomain(inc)
-    x=gens(R)
+function lift_orbit_representatives(x, ImV_i, p, poly, q, inv_poly, f_i, orbits, GL_vec)
     # W_i=V/V_i, f_i = V/V_{i+1}-> V/V_i, pi = V->W_i 
     # inc and inv_inc go between V and R_d = homogeneous_component of degree d
     # orbits = set in V/V_i
+    
     new_orbits = Set()
 
-    Im_i=Set(collect(ImV_i))
+    subIm_i=Set(collect(ImV_i)[1])
+    Im_i=ImV_i[2].(subIm_i)
 
-    for vec in orbits
-        
-        stabilizing_subgroup = Vector{FqMatrix}()
-        for A in GL_vec
-            if is_stabilizing(A)
-                push!(stabilizing_subgroup,A)
+    orbits_chunks = Iterators.partition(orbits, cld(length(orbits), Threads.nthreads()))
+
+    tasks = map(orbits_chunks) do chunk
+        Threads.@spawn begin
+            partial_orbit_reps = Set()
+            for vec in chunk
+                preim = preimage(p,vec)
+                preim_poly = poly(preim)
+                stabilizing_subgroup = Vector{FqMatrix}()
+                for A in GL_vec
+                    if is_stabilizing(A, vec, preim_poly, p, x, inv_poly)
+                        push!(stabilizing_subgroup,A)
+                    end
+                end
+
+                lift = preimage(f_i, vec) # preimage in V/V_{i+1}
+                coset = Set(lift+v for v in Im_i)
+
+                while !isempty(coset)
+                    g=first(coset)
+                    g_orbits = Set()
+                    push!(partial_orbit_reps, g)
+                    for A in stabilizing_subgroup
+                        f=poly(preimage(q,g))
+                        y=A*x
+                        h=f(y...)
+                        orbit_vec = q(inv_poly(h))
+                        push!(g_orbits, orbit_vec)
+                    end
+                    setdiff!(coset,g_orbits)
+                end
             end
-        end
-
-
-        lift = preimage(f_i, vec) # preimage in V/V_{i+1}
-        
-        coset = lift .+ Im_i
-
-        while !isempty(coset)
-            g=first(coset)
-            g_orbits = Set()
-            push!(new_orbits, g)
-            for A in stabilizing_subgroup
-                f=inc(preimage(q,g))
-                y=A*x
-                h=f(y...)
-                orbit_vec = q(inv_inc(h))
-                push!(g_orbits, orbit_vec)
-            end
-            setdiff!(coset,g_orbits)
+            return partial_orbit_reps
         end
     end
+    union!(new_orbits, (fetch.(tasks))...)
     return new_orbits
 end
 
 
 function projective_hypersurface_equivalence_classes_from_filtration(F, a, n, d; waring_samples=48, basis_samples=100, verbose=false)
+    
 
     head = _chain_constructor(F, a, n, d; waring_samples, basis_samples, verbose)
     rel_dim, filtration = _chain_finder(head)
+    # Considering the filtration ordered as V=V_1 > V_2 > V_3 > ... > V_{end-2} > V_{end-1} > 0
+
+    j=0
+    while length(filtration) == 2 || dim((filtration[end].object)[1]) != 0
+        head = _chain_constructor(F, a, n, d; waring_samples, basis_samples, verbose)
+        rel_dim, filtration = _chain_finder(head)
+        if verbose == true
+            println("Failed to find a good chain, trying again.")
+        end
+        if j == 10
+            println("Failed to find invariant submodules. Is this module irreducible?")
+            return
+        end
+        j+=1
+    end
+
     V, poly = head.object
+    R=codomain(poly)
+    x=gens(R)
+
+    inv_poly = inv(poly)
 
     if verbose == true
         println("Found chain with maximal relative dimension = ", rel_dim)
@@ -385,10 +420,11 @@ function projective_hypersurface_equivalence_classes_from_filtration(F, a, n, d;
 
     orbits = Set()
 
-    #Need to add some kind of check for if there's just a one-step filtration or irreducible
     quotients = []
     for i=1:length(filtration)-2
         push!(quotients,quo(V,(filtration[end-i].object)[1]))
+        # Storing V/V_{end-i} at index i
+        # So V/V_{end-1} to V/V_2
     end
 
     projection_maps = []
@@ -399,42 +435,72 @@ function projective_hypersurface_equivalence_classes_from_filtration(F, a, n, d;
         gen_images = [quo_target(preimage(quo_source, v)) for v in gens(Source_quo)]
         pi = ModuleHomomorphism(Source_quo, Target_quo, gen_images)
         push!(projection_maps, pi)
+        # Storing f_i : V/V_{end-i} ->> V/V_{end-i-1} at index i
     end
 
-    # Need to make images of quotients here.
+    images = []
+    for i=2:length(filtration)-2
+        push!(images, image(compose((filtration[end-i].object)[2], quotients[i-1][2])))
+        # Storing V_{end-i}/V_{end-i+1} at index i
+    end
 
-    # Find orbits in V/V_1, add to orbits.
-    #
-    #
-    #
-    
-    
 
-    #could possibly get n,d from filt?
-    #
-    # I think the following works:
-    # take filtration 0<V_k<V_(k-1)<...<V_2<V_1<V
-    # Compute V->V/V_k->V/V_(k-1)->...->V/V_2->V/V_1
-    # Maybe put in a list
-    #
-    # Make a set S of 0 in V/V_1
-    # 
-    # new func(set_of_orbit_reps, Maybe V/V_(i+1)->V/V_i)
-    #   for element in orbit reps set
-    #       make PGL stabilizing subgroup
-    #
-    #   do orbit find on coset of V/V_i with stabilizing subgroup
-    #   (returns a set. Splat the set into a bigger set).
-    #
-    #   return set_of_orbit_reps
-    #
-    #   for i=1:k+1
-    #       S=func(S,V/V_(i+1)->V/V_i)
-    #
-    #   return S
-    #   
+    if verbose == true
+        println("Starting stage #1 -- finding orbits in V/V_1")
+    end
+
+    # Find orbits in V/V_2, add to orbits.
+    W_2, f_2 = quotients[end]
+    starting_vecs = Set(collect(W_2))
+    while !isempty(starting_vecs)
+        vec = first(starting_vecs)
+        push!(orbits,vec)
+        orbit_of_vec = Set()
+        
+        lift = preimage(f_2, vec)
+        f = poly(lift)
+        
+        GL_chunks = Iterators.partition(GL_vec, cld(length(GL_vec), Threads.nthreads()))
+
+        tasks = map(GL_chunks) do chunk
+            Threads.@spawn begin
+                chunk_orbit = Set()
+                for A in chunk
+                    y = A*x
+                    g = f(y...)
+                    g_vec = inv_poly(g)
+                    orbit_vec = f_2(g_vec)
+                    push!(chunk_orbit, orbit_vec)
+                end
+                return chunk_orbit
+            end
+        end
+        union!(orbit_of_vec, (fetch.(tasks))...)
+        setdiff!(starting_vecs,orbit_of_vec)
+    end
+
+    if verbose == true
+        println("Starting stage #2 -- lifting orbits along chain")
+    end
+    
+    for i in (length(projection_maps)):-1:1
+        ImV_i = images[i]
+        f_i = projection_maps[i]
+        p = quotients[i+1][2]
+        q = quotients[i][2]
+        orbits = lift_orbit_representatives(x, ImV_i, p, poly, q, inv_poly, f_i, orbits, GL_vec)
+    end
+
+    if verbose == true
+        println("Starting stage #3 -- finding reprsentatives in V")
+    end
+
+    pi = quotients[1][2]
+    V_fin = (filtration[end-1].object)
+    id = identity_map(V)
+    orbits=lift_orbit_representatives(x,V_fin,pi, poly, id, inv_poly, pi, orbits, GL_vec)
+    poly_reps = Set(poly(v) for v in orbits)
+    delete!(poly_reps, R(0))
+
+    return poly_reps
 end
-
-#TODO: 1) Need a function that makes an action matrix on a quotient vector space
-# 2) Need a function that runs the algorithm "recursively" on a filtration
-# 3) Need a function filters through PGL and finds the stabilizer of an action matrix
